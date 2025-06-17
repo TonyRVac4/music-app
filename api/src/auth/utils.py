a1cac6a28650ec5e2536092a12dd3982258501e7
tony <technicalprogram4658@gmail.com>
2025-06-17 19:45:37 +0300

    Add: async redis client Add: sync func for sending emails

--- api/src/auth/utils.py
+++ api/src/auth/utils.py
@@ -1,13 +1,18 @@
+import smtplib
+import email
 from uuid import uuid4
 from datetime import datetime, timedelta
 
-from passlib.context import CryptContext
 from jose import jwt
+import redis.asyncio as redis
+from passlib.context import CryptContext
 
 from api.src.config import settings
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+async_redis_client = redis.from_url(settings.redis_db_url)
 
 
 def get_password_hash(password: str) -> str:
@@ -32,3 +37,16 @@ def create_jwt(payload: dict, token_type: str, expires_minutes: int) -> str:
     )
 
     return jwt.encode(payload, key=settings.JWT_KEY, algorithm=settings.JWT_ALGORITHM)
+
+
+def send_email(to_email: str, message: str) -> None:
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtpObj:
+        smtpObj.starttls()
+        smtpObj.login(user=settings.APP_EMAIL, password=settings.APP_EMAIL_PASSWORD)
+        m = email.message.Message()
+        m['From'] = settings.APP_EMAIL
+        m['To'] = to_email
+        m['Subject'] = "Music App email verification."
+
+        m.set_payload(message)
+        smtpObj.sendmail(from_addr=settings.APP_EMAIL, to_addrs=to_email, msg=m.as_string())
