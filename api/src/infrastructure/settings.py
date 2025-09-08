d8408c4ad6df762af19b2ac2d1a06eb2b9756359
tony <technicalprogram4658@gmail.com>
2025-09-08 16:32:51 +0300

    rework: env vars classes structure & .env.template

--- api/src/infrastructure/settings.py
+++ api/src/infrastructure/settings.py
@@ -1,131 +1,94 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 env_file_path: str = os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), ".env"
 )
 
 
-class AllSettings(BaseSettings):
-    DB_USER: str = "postgres"
-    DB_PASS: str = "postgres"
-    DB_HOST: str = "postgres"
-    DB_PORT: int = 5432
-    DB_NAME: str = "postgres"
-
-    REDIS_HOST: str = "redis"
-    REDIS_PORT: int = 6379
-    REDIS_APP_DB_NUM: int = 0
-    REDIS_CELERY_NUM: int = 1
-
-    S3_HOST: str = "s3"
-    S3_PORT: int = 9000
-    S3_ACCESS_KEY: str = "admin"
-    S3_SECRET_KEY: str = "admin"
-    BUCKET_NAME: str = "test_bucket"
-
-    JWT_KEY: str = "test_key"
-    JWT_ALGORITHM: str = "HS256"
-    TOKEN_TYPE_FILED_NAME: str = "type"
-    ACCESS_TOKEN_NAME: str = "access"
-    REFRESH_TOKEN_NAME: str = "refresh"
-    ACCESS_TOKEN_EXPIRES_MIN: int = 15
-    REFRESH_TOKEN_EXPIRES_MIN: int = 44640
-
-    APP_EMAIL: str
-    APP_EMAIL_PASSWORD: str
-    APP_HOST: str = "localhost"
-    APP_PORT: int = "8341"
-
-    VIDEO_DURATION_CONSTRAINT: float = 16
-    YOUTUBE_API_KEY: str
-    YOUTUBE_VISITOR_INFO1_LIVE: str
-
-    @property
-    def s3_config_dict(self) -> dict:
-        return {
-            "endpoint_url": f"http://{self.S3_HOST}:{self.S3_PORT}",
-            "access_key": self.S3_ACCESS_KEY,
-            "secret_key": self.S3_SECRET_KEY,
-            "bucket_name": self.BUCKET_NAME,
-        }
-
-    def redis_url(self, db_num: int) -> str:
-        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{db_num}"
-
-    model_config = SettingsConfigDict(
-        env_file=env_file_path,
-    )
-
-
-all_settings = AllSettings()
-
-
-class PostgresSettings:
-    username: str = all_settings.DB_USER
-    password: str = all_settings.DB_PASS
-    host: str = all_settings.DB_HOST
-    port: str = all_settings.DB_PORT
-    db_name: str = all_settings.DB_NAME
+class PostgresSettings(BaseSettings):
+    username: str = "postgres"
+    password: str = "postgres"
+    host: str = "postgres"
+    port: int = 5432
+    db_name: str = "postgres"
 
     url: str = f"postgresql+asyncpg://{username}:{password}@{host}:{port}/{db_name}"
 
 
-class RedisSettings:
-    host: str = all_settings.REDIS_HOST
-    port: str = all_settings.REDIS_PORT
-    app_db_num: int = all_settings.REDIS_APP_DB_NUM
-    celery_num: int = all_settings.REDIS_CELERY_NUM
+class RedisSettings(BaseSettings):
+    host: str = "redis"
+    port: int = 6379
+    app_db_num: int = 0
+    celery_broker_num: int = 1
 
-    app_url: str = all_settings.redis_url(app_db_num)
-    broker_url: str = all_settings.redis_url(celery_num)
-
-
-class S3Settings:
-    host: str = all_settings.S3_HOST
-    port: int = all_settings.S3_PORT
-    access_key: str = all_settings.S3_ACCESS_KEY
-    secret_key: str = all_settings.S3_SECRET_KEY
-    bucket_name: str = all_settings.BUCKET_NAME
-    config_dict: dict = all_settings.s3_config_dict
+    app_url: str = f"redis://{host}:{port}/{app_db_num}"
+    broker_url: str = f"redis://{host}:{port}/{celery_broker_num}"
 
 
-class AuthSettings:
-    jwt_key: str = all_settings.JWT_KEY
-    jwt_algorithm: str = all_settings.JWT_ALGORITHM
-    token_type_filed_name: str = all_settings.TOKEN_TYPE_FILED_NAME
-    access_token_name: str = all_settings.ACCESS_TOKEN_NAME
-    refresh_token_name: str = all_settings.REFRESH_TOKEN_NAME
-    access_token_expires_min: int = all_settings.ACCESS_TOKEN_EXPIRES_MIN
-    refresh_token_expires_min: int = all_settings.REFRESH_TOKEN_EXPIRES_MIN
+class S3Settings(BaseSettings):
+    host: str = "s3"
+    port: int = 9000
+    access_key: str = "admin"
+    secret_key: str = "admin"
+    bucket_name: str = "test_bucket"
+    http_prefix: str = "http"
+
+    @property
+    def config_dict(self) -> dict:
+        return {
+            "endpoint_url": f"{self.http_prefix}://{self.host}:{self.port}",
+            "access_key": self.access_key,
+            "secret_key": self.secret_key,
+            "bucket_name": self.buket_name,
+        }
 
 
-class EmailClientSettings:
-    email: str = all_settings.APP_EMAIL
-    password: str = all_settings.APP_EMAIL_PASSWORD
+class AuthSettings(BaseSettings):
+    jwt_key: str = "test_key"
+    jwt_algorithm: str = "HS256"
+    token_type_filed_name: str = "type"
+    access_token_name: str = "access"
+    refresh_token_name: str = "refresh"
+    access_token_expires_min: int = 15
+    refresh_token_expires_min: int = 44640
 
 
-class AppSettings:
-    host: str = all_settings.APP_HOST
-    port: int = all_settings.APP_PORT
+class EmailClientSettings(BaseSettings):
+    email: str = "example@gmail.com"
+    password: str = "password"
     verification_code_ttl_seconds: int = 600
 
 
-class YouTubeSettings:
-    video_duration_constraint: float = all_settings.VIDEO_DURATION_CONSTRAINT
-    api_key: str = all_settings.YOUTUBE_API_KEY
-    visitor_info1_live: str = all_settings.YOUTUBE_VISITOR_INFO1_LIVE
+class AppSettings(BaseSettings):
+    host: str = "localhost"
+    port: int = "8341"
 
 
-class Settings:
-    postgres = PostgresSettings()
-    redis = RedisSettings()
-    s3 = S3Settings()
-    auth = AuthSettings()
-    email_client = EmailClientSettings()
-    app = AppSettings()
-    youtube = YouTubeSettings()
+class YouTubeSettings(BaseSettings):
+    video_duration_constraint: float = 16.0
+    api_key: str = "Some API key"
+    visitor_info1_live: str = "Some visitor key"
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="API__",
+        env_file=(env_file_path, ".env", ".env.local"),
+        env_file_encoding="utf-8",
+        # разделитель для вложенных значений
+        env_nested_delimiter="__",
+        # если значение есть берет из env если нет будет использовано значение по умолчанию
+        nested_model_default_partial_update=True,
+    )
+
+    postgres: PostgresSettings = PostgresSettings()
+    redis: RedisSettings = RedisSettings()
+    s3: S3Settings = S3Settings()
+    auth: AuthSettings = AuthSettings()
+    email_client: EmailClientSettings = EmailClientSettings()
+    app: AppSettings = AppSettings()
+    youtube: YouTubeSettings = YouTubeSettings()
 
 
 settings = Settings()
